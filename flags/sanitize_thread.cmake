1067cd806b8d5e5e44c15cdce3f3558dbcf4b51a
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-03-23 22:19:13 +0800

    !!!TEST!!!

--- flags/sanitize_thread.cmake
+++ flags/sanitize_thread.cmake
@@ -11,10 +11,10 @@ include(polly_add_cache_flag)
 
 polly_add_cache_flag(CMAKE_CXX_FLAGS "-fsanitize=thread")
 polly_add_cache_flag(CMAKE_CXX_FLAGS "-fPIE")
-polly_add_cache_flag(CMAKE_CXX_FLAGS "-pie")
 polly_add_cache_flag(CMAKE_CXX_FLAGS "-g")
 
 polly_add_cache_flag(CMAKE_C_FLAGS "-fsanitize=thread")
 polly_add_cache_flag(CMAKE_C_FLAGS "-fPIE")
-polly_add_cache_flag(CMAKE_C_FLAGS "-pie")
 polly_add_cache_flag(CMAKE_C_FLAGS "-g")
+
+polly_add_cache_flag(CMAKE_EXE_LINKER_FLAGS "-pie")
